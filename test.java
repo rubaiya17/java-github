4c86702eb95e280b589fdb63bce6ac284126dbeb
Rubaiya <rsjenibhuiyan@gmail.com>
2025-09-14 02:01:10 +0600

    change

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello jeni");
-         System.out.println("Hello rubaiya");
+        System.out.println("Hello  world");
+        System.out.println("Hello rubaiya");
     }
 }
