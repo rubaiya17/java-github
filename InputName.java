968b0de74b2136a83fa72a57cb9d4ee2dc7666b8
Rubaiya <rsjenibhuiyan@gmail.com>
2025-09-17 21:09:43 +0600

    name input

---
+++ InputName.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class InputName {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Input Your Name :");
+    String name = sc.nextLine();
+    System.out.println("Hello, \"" + name + "\"!");
+    
+}
+
+
+}
